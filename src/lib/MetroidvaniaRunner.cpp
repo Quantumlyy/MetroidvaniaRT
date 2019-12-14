f517df4d088158958f1b87e044ffef65598edf54
Nejc <ethoarx@gmail.com>
2019-12-14 22:22:27 +0100

    Init

---
+++ src/lib/MetroidvaniaRunner.cpp
@@ -0,0 +1,1 @@
+#include "MetroidvaniaRunner.h"
---
+++ src/lib/MetroidvaniaRunner.h
@@ -0,0 +1,17 @@
+#ifndef METROIDVANIART_METROIDVANIARUNNER_H
+#define METROIDVANIART_METROIDVANIARUNNER_H
+
+#include "../../dependencies/NovelRT/src/NovelRunner.h"
+
+namespace MetroidvaniaRT {
+  /**
+   * The base class for creating a Metroidvania style game
+   */
+  class MetroidvaniaRunner : public NovelRT::NovelRunner {
+  public:
+  protected:
+  private:
+  };
+}
+
+#endif // METROIDVANIART_METROIDVANIARUNNER_H
